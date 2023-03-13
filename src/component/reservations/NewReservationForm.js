2ad4ecc60f3a4f3d7790e25b1ad2d1659ab0933b
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-03-13 20:08:15 +0530

    Add Reservation pages

---
+++ src/component/reservations/NewReservationForm.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { createReservationAPI } from '../../helpers/api';
+import isUserSigned from '../../helpers/auth';
+import { getBooksThunk } from '../../redux/books/booksSlice';
+
+const NewReservationForm = () => {
+  // eslint-disable-next-line react/prop-types, no-unused-vars
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // Authenticate the user
+  useEffect(() => {
+    if (!isUserSigned()) {
+      navigate('/signin');
+    }
+    dispatch(getBooksThunk());
+  }, [dispatch, navigate]);
+
+  const { books = null } = useSelector((state) => state.books);
+
+  // Extract the book id from the URL
+  const { bookId } = useParams();
+
+  // Set initial fields values
+  const [reservation, setReservation] = useState({
+    book_id: bookId || 0,
+    date: '',
+    city: '',
+  });
+
+  // Set message
+  const [msg, setMsg] = useState({
+    type: '',
+    text: '',
+  });
+
+  // Send the reservation details to the API
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Extract user details
+    const { id: userId } = JSON.parse(localStorage.getItem('user'));
+    createReservationAPI(userId, reservation)
+      .then((response) => {
+        if (response.status === 200) {
+          setMsg({ type: 'msg-sucss', text: 'The Book has been ordered' });
+        } else {
+          setMsg({ type: 'msg-fail', text: 'Something went wrong!' });
+        }
+      });
+  };
+
+  // Update the state after each change in the fields values
+  const handleFieldChange = (e) => {
+    const { name, value } = e.target;
+    setReservation({ ...reservation, [name]: value });
+  };
+  return (
+    <div id="rsrv-page">
+      <h1>BOOK ONE OF OUR BOOKS FOR A TEST DRIVE</h1>
+      <form>
+        <select name="book_id" value={bookId} onChange={handleFieldChange}>
+          {books && books.map((book) => (
+            <option key={book.id} value={book.id}>{book.name}</option>
+          ))}
+        </select>
+        <input type="text" name="city" placeholder="city" value={reservation.city} onChange={handleFieldChange} />
+        <input type="date" name="dateofbooking" placeholder="Date of Booking" value={reservation.date_of_booking} onChange={handleFieldChange} />
+        <input type="date" name="dateofdelivery" placeholder="Date of Delivery" value={reservation.date_of_delivery} onChange={handleFieldChange} />
+        <input className="buttn" type="submit" value="Order Now" onClick={handleSubmit} />
+      </form>
+      <span className={msg.type}>{ msg.text }</span>
+    </div>
+  );
+};
+
+export default NewReservationForm;
---
+++ src/component/reservations/Reservations.js
@@ -0,0 +1,63 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getReservationsThunk } from '../../redux/reservationsSlice';
+import locationIcon from '../../img/location_icon.png';
+import calendarIcon from '../../img/calendar_icon.png';
+import { getBooksThunk } from '../../redux/books/booksSlice';
+import isUserSigned from '../../helpers/auth';
+
+const Reservations = () => {
+  const { reservations } = useSelector((state) => state.reservations);
+  const { books = null } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    let userId;
+    if (!isUserSigned()) {
+      navigate('/signin');
+    } else {
+    // Extract user details
+      const { id } = JSON.parse(localStorage.getItem('user'));
+      userId = id;
+    }
+    dispatch((getBooksThunk()));
+    dispatch((getReservationsThunk(userId)));
+  }, [dispatch, navigate]);
+  const getBook = (bookId) => books.find((book) => book.id === bookId);
+  return (
+    <main id="resevations-page">
+      <h1>Your Reservations</h1>
+      <section id="resevations-list">
+        {books && reservations && reservations.map((reservation) => {
+          const book = getBook(reservation.book_id);
+          return (
+            <div key={reservation.id} className="mycard">
+              <h2>
+                {book.title}
+                {' '}
+                {book.author}
+              </h2>
+              <div className="mycard-footer">
+                <div className="myrow">
+                  <img className="myicon" src={locationIcon} alt="City name" />
+                  {reservation.city}
+                </div>
+                <div className="myrow">
+                  <img className="myicon" src={calendarIcon} alt="Date of Booking" />
+                  {reservation.date_of_booking}
+                </div>
+                <div className="myrow">
+                  <img className="myicon" src={calendarIcon} alt="Date of Delivery" />
+                  {reservation.date_of_delivery}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
+};
+
+export default Reservations;
